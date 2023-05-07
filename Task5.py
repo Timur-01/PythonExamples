2fa4da1d3b0bc6b272798d4b016548062c853445
Timur <cpdbox@mail.ru>
2023-05-07 11:17:12 +0300

    Task5.py

---
+++ Task5.py
@@ -0,0 +1,10 @@
+# iVan = int(input())
+# jVan = int(input())
+# print (iVan - 1 + jVan) 
+
+i = int (input())
+j = int (input())
+if i != j:
+    print (i+j-1)
+else:
+    print ('без дополнительной информации это сделать невозможно ')
